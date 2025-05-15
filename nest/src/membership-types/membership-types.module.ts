9344c1e1e754eaa753500e90b1b48066a259459a
Matej Rolko <cph-mr670@cphbusiness.dk>
2025-05-15 19:22:26 +0200

    modules fix

--- nest/src/membership-types/membership-types.module.ts
+++ nest/src/membership-types/membership-types.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MembershipTypesService } from './membership-types.service';
 import { MembershipTypesController } from './membership-types.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MembershipType } from './entities/membership-type.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MembershipType])],
   controllers: [MembershipTypesController],
   providers: [MembershipTypesService],
 })
--- nest/src/wash-types/wash-types.module.ts
+++ nest/src/wash-types/wash-types.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WashTypesService } from './wash-types.service';
 import { WashTypesController } from './wash-types.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WashType } from './entities/wash-type.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([WashType])],
   controllers: [WashTypesController],
   providers: [WashTypesService],
 })
