bf276af7dfe0e86deb13fe4f271ab20ced71dd18
Friggies <85342562+Friggies@users.noreply.github.com>
2025-05-25 18:21:00 +0200

    added tanstack provider

--- native/app/_layout.tsx
+++ native/app/_layout.tsx
@@ -1,11 +1,17 @@
+import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
 import { Slot } from 'expo-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
     <Provider store={store}>
-      <Slot />
+      <QueryClientProvider client={queryClient}>
+        <Slot />
+      </QueryClientProvider>
     </Provider>
   );
 }
